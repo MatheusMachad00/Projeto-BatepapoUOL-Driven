1ab3ee73630e57b88463dcb3d795e51c997fb8fb
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-09 14:26:23 -0300

    Function sendMessage created

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,30 @@
-/*const par = document.querySelector('p')
+let yourName = null;
+function chooseAName() {
+    let name = prompt("Digite seu nome: ");
+    yourName = name;
+}
 
-button.addEventListener('click', () => {
-    const date = new Date();
-    const local = date.toLocaleTimeString('pt-br');
-    par.innerHTML = local
-}) */
+const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+promise.then(generateMessage);
+
+/*function generateMessage(serverAwnser) {
+    var userName = document.querySelector(".")
+    var messageText = document.querySelector(".")
+    var messageTime = document.querySelector(".")
+    var messageTo = document.querySelector(".")
+    
+    userName.innerHTML = 
+} */
+
+/*function waitAwnser(serverAwnser) {
+    console.log(serverAwnser.data);
+} */
+
+function sendMessage() {
+    const main = document.querySelector('main');
+    message = document.querySelector('footer input').value;
+    main.innerHTML += `<div class="message" data-identifier="message">
+    <p class="date-time">(09:21:45)</p>
+    <p><strong class="username">${yourName}</strong> para <strong class="messageTo">joão</strong>: ${message}</p>
+</div>`;
+}
