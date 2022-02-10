09b4739148e82dc04febb9feeed625f2a969d71d
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-10 11:34:28 -0300

    Scroll function created

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,71 @@ function chooseAName() {
 const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
 promise.then(generateMessage);
 
-/*function generateMessage(serverAwnser) {
-    var userName = document.querySelector(".")
-    var messageText = document.querySelector(".")
-    var messageTime = document.querySelector(".")
-    var messageTo = document.querySelector(".")
+let userName = null;
+let messageText = null;
+let messageTime = null;
+let messageTo = null;
+
+function generateMessage(serverAwnser) {
+    /*let userName = document.querySelector(".username")
+    let messageText = document.querySelector(".messageTo")
+    let messageTime = document.querySelector(".date-time")
+    let messageTo = document.querySelector(".message") */
     
-    userName.innerHTML = 
-} */
-
-/*function waitAwnser(serverAwnser) {
     console.log(serverAwnser.data);
+
+    userName = serverAwnser.data.from;
+    messageTo = serverAwnser.data.to;
+    messageText = serverAwnser.data.text;
+    messageTime = serverAwnser.data.time;
+    console.log(userName);
+    console.log(messageText);
+    console.log(messageTime);
+    console.log(messageTo);
+}
+
+/*function serverMessages(serverAwnser) {
+    userName = serverAwnser.data.from;
+    messageText = serverAwnser.data.text;
+    messageTime = serverAwnser.data.time;
+    messageTo = serverAwnser.data.to;
+
+    const main = document.getElementById("#main");
+    main.innerHTML += `
+    <div class="message msgv" data-identifier="message">
+    <p class="date-time">(${messageTime})</p>
+    <p><strong class="username">${userName}</strong> para <strong class="messageTo">${messageTo}</strong>: ${messageText}</p>
+</div>`;
 } */
 
-function sendMessage() {
+function scroll() {
+    let lastMessage = document.querySelector('.msg:last-child');
+    lastMessage.scrollIntoView();
+}
+
+function sendUserMessage() {
+    var d = new Date();
+    var n = d.toLocaleTimeString();
     const main = document.querySelector('main');
     message = document.querySelector('footer input').value;
-    main.innerHTML += `<div class="message" data-identifier="message">
-    <p class="date-time">(09:21:45)</p>
-    <p><strong class="username">${yourName}</strong> para <strong class="messageTo">joão</strong>: ${message}</p>
+    main.innerHTML += `
+    <div class="message msg" data-identifier="message">
+    <p class="date-time">(${n})</p>
+    <p><strong class="username">${yourName}</strong> para <strong class="messageTo">todos</strong>: ${message}</p>
 </div>`;
+scroll();
 }
+
+/*api:
+{
+from: "João",
+to: "Todos",
+text: "entra na sala...",
+type: "status",
+time: "08:01:17"
+}
+*/
+
+/*function generateMessage(serverAwnser) {
+    console.log(serverAwnser.data);
+} */
