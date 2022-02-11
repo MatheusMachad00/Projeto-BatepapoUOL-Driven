109f128ad8194b99ed707913326e8d833ad36f4e
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-11 16:18:07 -0300

    Function to check if the name is valid created

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,29 @@
 let yourName = null;
+let objName = {};
+
 function chooseAName() {
     let name = prompt("Digite seu nome: ");
+    let myObject = {
+        name: name
+    };
     yourName = name;
+    objName = myObject;
+    const request = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', objName);
+    request.then(validName);
+    request.catch(chooseANameAgain);
+}
+
+function validName(resposta) {
+    if (resposta.status === 200) {
+        alert("Nome disponível!");
+    }
+}
+
+function chooseANameAgain(resposta) {
+    if (resposta[2] !== 200) {
+        alert("Nome indiponível, por favor escolha outro nome."); 
+    }
+    chooseAName()
 }
 
 const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
@@ -14,22 +36,22 @@ let messageTime = null;
 let status = null;
 
 function generateMessage(serverAwnser) {
-    for (i = 0; i < 100; i++){
-        console.log(i);
-        console.log(serverAwnser.data[i].from);
+    for (i = 0; i < 100; i++) {
+        /*console.log(i);
+        console.log(serverAwnser.data[i].from); */
         userName = serverAwnser.data[i].from;
         messageTo = serverAwnser.data[i].to;
         messageText = serverAwnser.data[i].text;
-        messageTime = serverAwnser.data[i].time;        
+        messageTime = serverAwnser.data[i].time;
         statusMsg = serverAwnser.data[i].type;
     }
-    
+
 }
 
 var interval = window.setInterval(checkMsgAPI, 3000);
 
 function checkMsgAPI() {
-    serverMessages();
+    //serverMessages(); //REMOVER O COMENTÁRIO PARA A FUNÇÃO VOLTAR AO NORMAL
 }
 
 function serverMessages() {
@@ -39,13 +61,16 @@ function serverMessages() {
     <p class="date-time">(${messageTime})</p>
     <p><strong class="username">${userName}</strong> para <strong class="messageTo">${messageTo}</strong>: ${messageText}</p>
 </div>`;
-scroll();
+    scroll();
 }
 
 function scroll() {
     let lastMessage = document.querySelector('.msg:last-child');
     lastMessage.scrollIntoView();
 }
+
+let UserMsgTime = null;
+let userMsg = null;
 
 function sendUserMessage() {
     var d = new Date();
@@ -59,6 +84,10 @@ function sendUserMessage() {
 </div>`;
     scroll();
 }
+
+let objMsg = {};
+
+
 
 /*let classes;
 function msgStatus {
