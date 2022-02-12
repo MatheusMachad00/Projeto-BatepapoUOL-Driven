63f36ba72a54646d75f204488f43e32a93d183b9
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-12 16:24:40 -0300

    Function to check user status partially created

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,57 @@
 let yourName = null;
-let objName = {};
+let objName = { name: '' };
+
+window.onload = chooseAName ();
 
 function chooseAName() {
-    let name = prompt("Digite seu nome: ");
-    let myObject = {
-        name: name
-    };
-    yourName = name;
-    objName = myObject;
+    objName.name = prompt("Digite seu nome: ");
+    yourName = objName.name;
     const request = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', objName);
     request.then(validName);
     request.catch(chooseANameAgain);
 }
 
-function validName(resposta) {
-    if (resposta.status === 200) {
+function validName(serverAwnser) {
+    if (serverAwnser.status === 200) {
         alert("Nome disponível!");
+    }
+    //checkUserStatus();
+}
+
+function chooseANameAgain(serverAwnser) {
+    if (serverAwnser.status !== 200) {
+        alert("Nome indiponível, por favor escolha outro nome.");
+    }
+    chooseAName();
+}
+
+/* function checkUserStatus() {
+    const request = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', objName);
+    request.then(userOnline);
+    request.catch(userOffline);
+}
+
+function userOnline(requestAwnser) {
+    if (serverAwnser.status === 200) {
+        checkMsgAPI();
+        console.log(requestAwnser)
     }
 }
 
-function chooseANameAgain(resposta) {
-    if (resposta[2] !== 200) {
-        alert("Nome indiponível, por favor escolha outro nome."); 
+function userOffline() {
+    if (serverAwnser.status !== 200) {
+        const main = document.querySelector("main");
+        main.innerHTML += `
+        <div class="message msg inOut" data-identifier="message">
+        <p class="date-time">(${messageTime})</p>
+        <p><strong class="username">${userName}</strong> saiu da sala...</p>
+        </div>`;
+        scroll();
     }
     chooseAName()
 }
+
+var userOnline = window.setInterval(checkUserStatus, 5000); */
 
 const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
 promise.then(generateMessage);
@@ -37,8 +64,6 @@ let status = null;
 
 function generateMessage(serverAwnser) {
     for (i = 0; i < 100; i++) {
-        /*console.log(i);
-        console.log(serverAwnser.data[i].from); */
         userName = serverAwnser.data[i].from;
         messageTo = serverAwnser.data[i].to;
         messageText = serverAwnser.data[i].text;
@@ -51,7 +76,7 @@ function generateMessage(serverAwnser) {
 var interval = window.setInterval(checkMsgAPI, 3000);
 
 function checkMsgAPI() {
-    //serverMessages(); //REMOVER O COMENTÁRIO PARA A FUNÇÃO VOLTAR AO NORMAL
+    serverMessages(); //REMOVER O COMENTÁRIO PARA A FUNÇÃO VOLTAR AO NORMAL
 }
 
 function serverMessages() {
@@ -69,8 +94,16 @@ function scroll() {
     lastMessage.scrollIntoView();
 }
 
-let UserMsgTime = null;
+/* let UserMsgTime = null;
 let userMsg = null;
+let msgTo = 'Todos';
+let msgType = 'message';
+let objMsg = {
+    from: yourName,
+    to: msgTo,
+    text: userMsg,
+    type: msgType
+}; */
 
 function sendUserMessage() {
     var d = new Date();
@@ -83,9 +116,11 @@ function sendUserMessage() {
     <p><strong class="username">${yourName}</strong> para <strong class="messageTo">todos</strong>: ${message}</p>
 </div>`;
     scroll();
+    /* userMsg = document.querySelector('footer input').value;
+    UserMsgTime = n;
+    console.log(objMsg); */
 }
 
-let objMsg = {};
 
 
 
